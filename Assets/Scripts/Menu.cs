b84b0c512da3ebebc609989448e1fd63f65ccb5f
Fuze5k <51718249+Fuze5k@users.noreply.github.com>
2020-03-26 19:59:27 +0200

    Add files via upload

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Continue()
+    {
+
+        //SceneManager.LoadScene("SampleScene");
+    }
+    public void LoadSetting()
+    {
+        SceneManager.LoadScene("Setting");
+    }
+}
---
+++ Assets/Scripts/Setting.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Setting : MonoBehaviour
+{
+    private bool isFullScreen = Screen.fullScreen;
+    private Resolution[] rsl;
+    private List<string> resolutions;
+    public Dropdown resolutiondropdown;
+    public void FullScreenToggle()
+    {
+        isFullScreen = !isFullScreen;
+        Screen.fullScreen = isFullScreen;
+    }
+    public void Exit()
+    {
+        SceneManager.UnloadScene("Setting");
+    }
+    public void Awake()
+    {
+        resolutions = new List<string>();
+        rsl = Screen.resolutions;
+        foreach (var i in rsl)
+        {
+            resolutions.Add(i.width + "x" + i.height);
+        }
+        resolutiondropdown.ClearOptions();
+        resolutiondropdown.AddOptions(resolutions);
+    }
+    public void Resolution(int r)
+    {
+        Screen.SetResolution(rsl[r].width, rsl[r].height, isFullScreen);
+    }
+}
