bec677291be02139e42d1562714117031d5242bf
Ilya Kusik <kusikilya2001@outlook.com>
2020-03-29 01:58:28 +0200

    Commit

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -1,53 +1,4 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.SceneManagement;
-
-//public class PlayerMovementScript : MonoBehaviour
-//{
-//    public RoomSpawner rSp;
-//    public Rigidbody2D rb;
-//    public Gun gun;
-//    public float speed;
-//    public float jump;
-
-//    private float HorizMove;
-//    private bool isJump;
-
-//    private void Start() {
-//        this.transform.position = rSp.getCurrentLocationAll();
-//    }
-
-//    void Update()
-//    {
-//        HorizMove = Input.GetAxis("Horizontal");
-//        if (Input.GetButtonDown("Jump"))
-//        {
-//            isJump = true;
-//        }
-
-//        if (Input.GetButtonDown("Fire1"))
-//        {
-//            gun.Attack();
-//        }
-
-//        if (Input.GetButtonDown("Fire2"))
-//        {
-//            gun.ChangeState();
-//        }
-//    }
-
-//    private void FixedUpdate()
-//    {
-//        if (isJump)
-//        {
-//            rb.velocity = new Vector2(HorizMove * speed, jump);
-//            isJump = false;
-//        }
-//        rb.velocity = new Vector2(HorizMove * speed, rb.velocity.y);
-//    }
-//}
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -69,7 +20,7 @@ public class PlayerMovementScript : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        this.transform.position = rSp.getCurrentLocationAll();
+        if(rSp != null) this.transform.position = rSp.getCurrentLocationAll();
     }
 
     void Update()
