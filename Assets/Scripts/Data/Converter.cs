a593635b337b8c06519155eceaeff7127e6e204c
unknownMusician <serega3assassin2002@gmail.com>
2020-04-23 19:33:54 +0300

    Started Converter

---
+++ Assets/Scripts/Data/Converter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Converter : MonoBehaviour
+{
+    private static RoomSpawner roomSpawner;
+    public static Image GetMinimapImage() {
+        GameObject[] rooms = roomSpawner.GetMiniMapMatrix();
+        // HUI;
+    }
+}
