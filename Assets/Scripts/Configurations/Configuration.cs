3ee434e3f04b4ec68a8420fafc945ae442d037fb
unknownMusician <serega3assassin2002@gmail.com>
2020-06-29 12:30:32 +0300

    added some Weapon configs

--- Assets/Scripts/Configurations/Configuration.cs
+++ Assets/Scripts/Configurations/Configuration.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class ConfigInitializer
+public static class Configuration
 {
     public static string _globalConfigPath = "Global.config";
+    public static string _weaponConfigPath = "Weapon.config";
 
     public static GlobalConfig Global;
+    public static WeaponConfig weapon;
 
     public static void Initialize()
     {
         Global = new GlobalConfig(_globalConfigPath);
+        weapon = new WeaponConfig(_weaponConfigPath);
     }
 }
