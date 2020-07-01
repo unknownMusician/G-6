4f80420047de43a36a04c7272d3f442080d24da7
Fuze5k <51718249+Fuze5k@users.noreply.github.com>
2020-07-01 12:58:04 +0300

    fix dropdown bug

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,24 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
+
 public class MainMenu : MonoBehaviour
 {
     public GameObject settingGameObject;
+    public GameObject thisGameObject;
     public AudioMixer audioGameObject;
     public GameObject gameUIGameObject;
+    public GameObject clear;
     public void PlayPressed()
     {
         DontDestroyOnLoad(settingGameObject);
         DontDestroyOnLoad(audioGameObject);
         DontDestroyOnLoad(gameUIGameObject);
+        DontDestroyOnLoad(clear);
 
         gameUIGameObject.active = true;
 
         SceneManager.LoadScene("Level 1");
     }
+
+    public void LoadSetting()
+    {
+        thisGameObject.active = false;
+        settingGameObject.active = true;
+        Setting.prap = true;
+    }
+
 
     void Start()
     {
--- Assets/Scripts/UI/Setting.cs
+++ Assets/Scripts/UI/Setting.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -12,6 +13,10 @@ public class Setting : MonoBehaviour
     private List<string> resolutions;
     public AudioMixer audiomixer;
     public Dropdown resolutiondropdown;
+    public static bool prap = false;
+    public GameObject mainMenu;
+    public GameObject setting;
+    public GameObject clear;
 
     public void FullScreenToggle()
     {
@@ -30,6 +35,17 @@ public class Setting : MonoBehaviour
         }
         resolutiondropdown.ClearOptions();
         resolutiondropdown.AddOptions(resolutions);
+    }
+
+    public void Exit()
+    {
+        setting.active = false;
+        if (prap)
+        {
+            mainMenu.active = true;
+            prap = false;
+        }
+        clear.SetActive(false);
     }
 
     public void Resolution(int r)
