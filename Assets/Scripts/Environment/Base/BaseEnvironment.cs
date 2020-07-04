7f8468589e4847a5094ae2a66189fa7fc2e7453d
Ilya Kusik <kusikilya2001@outlook.com>
2020-07-04 16:39:27 +0300

    Base-Interaction-Done
    #34

---
+++ Assets/Scripts/Environment/Base/BaseEnvironment.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseEnvironment : MonoBehaviour
+{
+
+    #region MonoBehaviourMethods
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    public abstract void Interact();
+}
