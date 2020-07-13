0d2a2ffcb3baa82bc7768d4ca58f36ef17f8f434
Ilya Kusik <kusikilya2001@outlook.com>
2020-07-13 10:05:42 +0300

    Configs fixed

--- Assets/Scripts/Configurations/WeaponConfig.cs
+++ Assets/Scripts/Configurations/WeaponConfig.cs
@@ -3,29 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponConfig : BaseConfig<WeaponConfigEntity> {
-    public struct Gun {
-        public struct Pistol {
-            int clipSize;
-            int pocketSize;
-            float standardBulletSpeed;
-            float standardBulletLifetime;
-            float standardBulletDamage;
-            float standardMeleeDamage;
-            float standardMaxThrowDamage;
-            float standardReloadTime;
-            float standardRecoilTime;
-            float standardThrowLoadTime;
-        }
-    }
-
-    public struct Melee {
-        public struct Sword {
-            float standardMeleeDamage;
-            float standardMaxThrowDamage;
-            float standardRecoilTime;
-            float standardThrowLoadTime;
-        }
-    }
 
     public WeaponConfig(string name) : base(name) {
 
