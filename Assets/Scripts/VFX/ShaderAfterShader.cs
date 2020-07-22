74a5953c782c3e5719bda9a5ff852a2b0bad657f
unknownMusician <serega3assassin2002@gmail.com>
2020-07-22 22:41:55 +0300

    added LIGHTING!!!

--- Assets/Scripts/VFX/ShaderAfterShader.cs
+++ Assets/Scripts/VFX/ShaderAfterShader.cs
@@ -36,8 +36,8 @@ public class ShaderAfterShader : MonoBehaviour {
 
     #region Overrided Methods
 
-    private void Update() {
-        Vector3 playerPos = Camera.main.WorldToScreenPoint(player.position);
+    private void OnRenderImage(RenderTexture src, RenderTexture dst) {
+        Vector3 playerPos = Camera.current.WorldToScreenPoint(player.position);
         Vector3 mousePos = Input.mousePosition;
         Vector4 plPos = new Vector4(playerPos.x / Camera.main.pixelWidth, playerPos.y / Camera.main.pixelHeight, 0, 0);
         Vector4 lkPos = new Vector4(mousePos.x / Camera.main.pixelWidth, mousePos.y / Camera.main.pixelHeight, 0, 0);
@@ -70,8 +70,46 @@ public class ShaderAfterShader : MonoBehaviour {
         materials[1].SetFloat("_ShowInColor", showInColor ? 1.0f : 0.0f);
         materials[1].SetFloat("_Intensity", intensity);
 
-        Graphics.Blit(rt, bufferRt, materials[0]);
+        Graphics.Blit(src, bufferRt, materials[0]);
         Graphics.Blit(bufferRt, finalRt, materials[1]);
+    }
+
+    private void Update() {
+        //Vector3 playerPos = Camera.main.WorldToScreenPoint(player.position);
+        //Vector3 mousePos = Input.mousePosition;
+        //Vector4 plPos = new Vector4(playerPos.x / Camera.main.pixelWidth, playerPos.y / Camera.main.pixelHeight, 0, 0);
+        //Vector4 lkPos = new Vector4(mousePos.x / Camera.main.pixelWidth, mousePos.y / Camera.main.pixelHeight, 0, 0);
+        //Vector4 diff4 = lkPos - plPos;
+        //Vector2 diff = new Vector2(diff4.x, diff4.y);
+        //float angle = (float)Mathf.Atan2(diff.y, diff.x);
+
+        //Vector4 centerOffset4 = new Vector4(centerOffset.x, centerOffset.y);
+        //Vector4 centerOffsetWichRotates4 = new Vector4(centerOffsetWichRotates.x, centerOffsetWichRotates.y);
+
+        //Vector4 resolution = new Vector4(Camera.main.pixelWidth, Camera.main.pixelHeight);
+
+        //materials[0].SetVector("_PlayerPos", plPos);
+        //materials[0].SetVector("_LookPos", lkPos);
+        //materials[0].SetFloat("_LookAngle", angle);
+        //materials[0].SetVector("_Resolution", resolution);
+        //materials[0].SetFloat("_ViewWidth", viewWidth);
+        //materials[0].SetVector("_Offset", centerOffset4);
+        //materials[0].SetVector("_OffsetRot", centerOffsetWichRotates4);
+        //materials[0].SetFloat("_ShowInColor", showInColor ? 1.0f : 0.0f);
+        //materials[0].SetFloat("_Intensity", intensity);
+
+        //materials[1].SetVector("_PlayerPos", plPos);
+        //materials[1].SetVector("_LookPos", lkPos);
+        //materials[1].SetFloat("_LookAngle", angle);
+        //materials[1].SetVector("_Resolution", resolution);
+        //materials[1].SetFloat("_ViewWidth", viewWidth);
+        //materials[1].SetVector("_Offset", centerOffset4);
+        //materials[1].SetVector("_OffsetRot", centerOffsetWichRotates4);
+        //materials[1].SetFloat("_ShowInColor", showInColor ? 1.0f : 0.0f);
+        //materials[1].SetFloat("_Intensity", intensity);
+
+        //Graphics.Blit(rt, bufferRt, materials[0]);
+        //Graphics.Blit(bufferRt, finalRt, materials[1]);
     }
 
     private void OnDestroy() {
