b10677b130405c55e5888e28a424b885331534d3
Fuze5k <51718249+Fuze5k@users.noreply.github.com>
2020-07-30 13:49:10 +0300

    add logic for save and load game

---
+++ Assets/Scripts/UI/SaveLoad/BaseSaveLoad.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using UnityEngine;
+
+namespace Assets.Scripts.UI.SaveLoad
+{
+
+    public class BaseSaveLoad<TData> where TData : class
+    {
+        protected string Path;
+        protected TData Data;
+
+        public BaseSaveLoad(string name)
+        {
+            Path = System.IO.Path.Combine(Application.streamingAssetsPath, name);
+        }
+
+        public void Serialize()
+        {
+            try
+            {
+                XmlSerializer formatter = new XmlSerializer(typeof(TData));
+
+                using (FileStream fs = new FileStream(Path, FileMode.OpenOrCreate))
+                {
+                    formatter.Serialize(fs, (TData)this.Data);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Logger.LogW(ex, $"Error with putting data in {typeof(TData)} save");
+            }
+
+        }
+        public void Deserialize()
+        {
+            try
+            {
+                XmlSerializer formatter = new XmlSerializer(typeof(TData));
+
+                using (FileStream fs = new FileStream(Path, FileMode.Open))
+                {
+                    this.Data = (TData)formatter.Deserialize(fs);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Logger.LogW(ex, $"Error with getting data in {typeof(TData)} load");
+            }
+
+        }
+
+
+    }
+
+}
---
+++ Assets/Scripts/UI/SaveLoad/SaveAndLoadGame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Assets.Scripts.UI.SaveLoad;
+using UnityEngine;
+
+public class SaveAndLoadGame : MonoBehaviour
+{
+    private static string path = DataSetDateTime.Local.ToString();
+
+    public static SaveLoad SaveLoad;
+    public static void SaveGame(string name)
+    {
+        SaveLoad = new SaveLoad(name + DataSetDateTime.Local.ToString() + ".sl");
+        SaveLoad.Serialize();
+    }
+    public static void LoadGame(string name)
+    {
+        SaveLoad = new SaveLoad(name);
+        SaveLoad.Deserialize();
+    }
+}
+
+public class SaveLoad : BaseSaveLoad<SaveLoadData>
+{
+    public SaveLoad(string path) : base(path)
+    {
+
+    }
+}
+
+[Serializable]
+public class SaveLoadData
+{
+
+}
