ca293e30646b6257fcf6b8ecd0bd9463d1f2a555
unknownMusician <serega3assassin2002@gmail.com>
2020-09-02 15:34:00 +0300

    Update EnvironmentBuilder.cs

--- Assets/Scripts/EnvironmentBuilder/EnvironmentBuilder.cs
+++ Assets/Scripts/EnvironmentBuilder/EnvironmentBuilder.cs
@@ -13,11 +13,18 @@ public class EnvironmentBuilder : MonoBehaviour {
     [SerializeField]
     private Vector2 roomSize = new Vector2(5, 5);
 
+    [SerializeField]
+    private List<GameObject> blocks = new List<GameObject>();
+
     public void OnClick(int blockID) {
         
     }
 
     private void OnDrawGizmos() {
         
+    }
+
+    private void FillBlocksMenu() {
+
     }
 }
