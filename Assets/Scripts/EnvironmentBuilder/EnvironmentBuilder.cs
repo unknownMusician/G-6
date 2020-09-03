e18b00646ce9610bdd90be2bbd4f623719a8108e
unknownMusician <serega3assassin2002@gmail.com>
2020-09-03 19:33:09 +0300

    timeScale

--- Assets/Scripts/EnvironmentBuilder/EnvironmentBuilder.cs
+++ Assets/Scripts/EnvironmentBuilder/EnvironmentBuilder.cs
@@ -86,6 +86,7 @@ public class EnvironmentBuilder : MonoBehaviour {
     }
 
     private void Start() {
+        Time.timeScale = 0;
         ShowBuildBarrier();
         FillMenu(blocksMenu, blocksPrefabs);
         FillMenu(objectsMenu, objectsPrefabs);
